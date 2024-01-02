c076a7ba03eedd694b2009bec580816f482b8027
Roshan maurya <roshanmaurya456@gmail.com>
2024-01-02 23:19:25 +0530

    js add

---
+++ script.js
@@ -0,0 +1,14 @@
+//let - isko hm locally use karte hai
+//var - isko hm globally use karte hai
+//const - isko kabhi bhi change nhi kiya ja sakta hai, 
+//i mean 1 baar declare ho gya to wah hr jagah same rahega
+
+//() - isko body bolte hai
+// {} - isko curly braces bola jata hai
+
+//ab hm 1 function bna k dekhte hai
+
+let maurya=()=>{
+    document.getElementById("sameer").style.backgroundColor="red";
+}
+
